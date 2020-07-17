75735c3be264d3e214c11c3ffaf0f008eb9fba06
Gadde-Nitish-Samanth <nitishgadde01@gmail.com>
2020-07-17 16:00:01 +0530

    implemented tabbed activities

---
+++ Reminder/app/src/main/java/com/example/reminder/Tab2.java
@@ -0,0 +1,20 @@
+package com.example.reminder;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+public class Tab2 extends Fragment {
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.frag2, container, false);
+
+        return root;
+    }
+}
